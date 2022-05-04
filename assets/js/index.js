a992738504a4779bfa8633d0cf23ea9af75d7cdf
Johnny Faria <johnnys.faria@gmail.com>
2022-05-04 18:00:53 -0300

    ajustes finais

--- assets/js/index.js
+++ assets/js/index.js
@@ -3,28 +3,19 @@ var a = 170;
 function verMais() {
   const points = document.getElementById('points');
   const moreText = document.getElementById('more');
-  const teste = document.getElementById('teste');
   a = a + 350;
-
-  if (points.style.display === 'none') {
-    points.style.display = 'inline';
-    moreText.style.display = 'none';
-  } else {
-    points.style.display = 'none';
-    moreText.style.display = 'inline';
-    teste.style.height = a + 'px';
-  }
+  points.style.display = 'none';
+  moreText.style.display = 'inline';
 }
 
 function criarTopico() {
   const criar = document.getElementById('criar');
   const teste = document.getElementById('teste');
-
-  if (criar.style.display === 'none') {
+  if (window.getComputedStyle(criar).getPropertyValue('display') === 'none') {
     criar.style.display = 'inline';
-    teste.style.display = 'none';
+    topic.style.display = 'none';
   } else {
     criar.style.display = 'none';
-    teste.style.display = 'flex';
+    topic.style.display = 'inline';
   }
 }
